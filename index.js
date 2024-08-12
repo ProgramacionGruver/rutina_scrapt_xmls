d8b8f09247c2652a9394084d2b60ced55060ad4e
Alexis Magdaleno <amagdaleno@gruver.mx>
2024-08-12 09:15:44 -0600

    Cambio horario

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ limit: '200mb', extended: true }))
 
 app.use(cors())
 
-cron.schedule('0 22 * * *', () => {
+cron.schedule('0 9,12,15,18 * * *', () => {
     manejoRutinaXML()
 })
 
